e7708d6ebd799b268ac24121ab25940b323cf7b0
Adwaithjikumar <142088539+axdwth@users.noreply.github.com>
2024-11-24 14:29:57 +0530

    addthree variable to a single variable

---
+++ javascript/Randomnumber/jsfile2.js
@@ -0,0 +1,34 @@
+/*let random = Math.random();
+otp = random;
+otp = Math.ceil(otp);
+
+let random2 = Math.random();
+otp2 = random2;
+otp2 = Math.floor(otp2);
+
+let random3 = Math.random();
+otp3 = random3;
+otp3 = Math.ceil(otp3);
+*/
+let otp;
+let otp2;
+let otp3;
+const genrate = document.getElementById("btngenarate");
+const countLabel2 = document.getElementById("countLabel2");
+genrate.onclick = function () {
+  let random = Math.random();
+  otp = random;
+  otp = Math.ceil(otp);
+
+  let random2 = Math.random();
+  otp2 = random2;
+  otp2 = Math.ceil(otp2);
+
+  let random3 = Math.random();
+  otp3 = random3;
+  otp3 = Math.ceil(otp3);
+  let finalotp;
+  finalotp = `${otp}${otp2}${otp3}`;
+  countLabel2.textContent = `your otp is ${finalotp}`;
+  console.log(`your otp is ${finalotp}`);
+};
