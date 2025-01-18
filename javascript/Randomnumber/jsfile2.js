bbd5e642097549f4145aba98897611b0244a719e
Adwaithjikumar <142088539+axdwth@users.noreply.github.com>
2025-01-18 12:46:40 +0530

    Initial commit

--- javascript/Randomnumber/jsfile2.js
+++ javascript/Randomnumber/jsfile2.js
@@ -16,17 +16,17 @@ let otp3;
 const genrate = document.getElementById("btngenarate");
 const countLabel2 = document.getElementById("countLabel2");
 genrate.onclick = function () {
-  let random = Math.random();
+  let random = Math.random() * 10;
   otp = random;
-  otp = Math.ceil(otp);
+  otp = Math.ceil(otp) * 6;
 
-  let random2 = Math.random();
+  let random2 = Math.random() * 10;
   otp2 = random2;
   otp2 = Math.ceil(otp2);
 
   let random3 = Math.random();
   otp3 = random3;
-  otp3 = Math.ceil(otp3);
+  otp3 = Math.ceil(otp3 * 6) + 9; //oooooooooo
   let finalotp;
   finalotp = `${otp}${otp2}${otp3}`;
   countLabel2.textContent = `your otp is ${finalotp}`;
